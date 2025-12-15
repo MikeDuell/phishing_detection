e74e742062d54dfb87b7d079ab0913923b586c58
MikeDuell <90914610+MikeDuell@users.noreply.github.com>
2025-12-15 11:26:38 +0000

    Update data source URL for training data

--- com624_code/email_dashboard.py
+++ com624_code/email_dashboard.py
@@ -95,7 +95,7 @@ if "training_data" not in st.session_state:
 print(len(st.session_state["training_data"]))
 if st.button("Train Model"):
     if len(st.session_state["training_data"]) == 0:
-        st.session_state["training_data"] = pd.read_csv('https://1drv.ms/x/c/6847b87d1a6f8284/IQDeuinhzuG-SYCjbtzTdZ5fAYDcp2OJzis-GMd5Ucq6mK0?e=WOiGVY',
+        st.session_state["training_data"] = pd.read_csv('https://onedrive.live.com/download?resid=AbCdEfGhIjKlMnOpQrStUvWxYz123',
                                                         encoding='ISO-8859-1',
                                                         dtype={"label": "int8", "urls": "int8"},
                                                         na_values=['na', 'NA', 'Unknown', ''])
