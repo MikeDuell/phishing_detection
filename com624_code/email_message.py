d0a8859642a5f444662734128e7a3a41a586b743
MikeDuell <90914610+MikeDuell@users.noreply.github.com>
2025-12-15 10:35:12 +0000

    Update email_message.py

--- com624_code/email_message.py
+++ com624_code/email_message.py
@@ -6,12 +6,6 @@ from base64 import urlsafe_b64decode
 
 SCOPES = ['https://mail.google.com/']
 
-import streamlit as st
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.oauth2.credentials import Credentials
-from base64 import urlsafe_b64decode
-
 
 
 def gmail_authenticate():
